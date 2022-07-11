894a2421e9c156b03268890eb11e915e3febb2fb
jsm8109jsm <jsm8109jsm@gmail.com>
2022-07-12 08:25:09 +0900

    useRef로 특정 DOM 선택하기

--- src/components/InputSample.js
+++ src/components/InputSample.js
@@ -1,12 +1,14 @@
 //inputSample.js
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 export default function InputSample() {
     const [inputs, setInputs] = useState({ //useState 객체 형태
         name: '',
         nickname: ''
     });
+
+    const nameInput = useRef(); //useRef, nameInput이라는 객체 생성
     const { name, nickname } = inputs; //비구조화 할당
 
     const onChange = (e) => {
@@ -16,7 +18,6 @@ export default function InputSample() {
             ...inputs, //spread 연산자
             [name]: value //name 값에 value 값을 덮어씌움(만약 name: value라고 하게 되면 name 문자열 자체에 들어가게 됨)
         }
-        console.log(name);
 
         setInputs(nextInputs);
     }
@@ -26,10 +27,11 @@ export default function InputSample() {
             name: '',
             nickname: ''
         })
+        nameInput.current.focus(); //ref가 nameInput인 DOM에 focus 함수 적용
     }
     return (
         <>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput}/> {/*ref 값을 nameInput으로 줌*/}
             <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
