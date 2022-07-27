9a1c86e37a90a78e7a48fb9803b900ddaa336047
jsm8109jsm <jsm8109jsm@gmail.com>
2022-07-27 14:36:45 +0900

    Counter.js 클래스형으로 바꾸기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,36 +1,36 @@
 //Counter.js
 
-import React, { useReducer } from  'react';
+import React, { Component } from 'react';
 
-function reducer(state, action){ //상태를 관리하는 로직
-    switch (action.type){
-        case 'INCREMENT':
-            return state + 1;
-        case 'DECREMENT':
-            return state - 1;
-        default:
-            // return state;
-            throw new Error('Unhandled action');
+class Counter extends Component {
+
+    state = {
+        counter: 0,
+        fixed: 1,
+    }
+
+    handleDecrease = () => {
+        this.setState(state => ({
+            counter: state.counter + 1
+        }))
+    }
+
+    handleIncrease = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.counter}</h1>
+                <button onClick={this.handleDecrease}>+1</button>
+                <button onClick={this.handleIncrease}>-1</button>
+                <span>{this.state.fixed}</span>
+            </div>
+        )
     }
 }
 
-export default function Counter() {
-    const [number, dispatch] = useReducer(reducer, 0); //useReducer
-    const onDecrease = () => {
-        dispatch({
-            type: 'INCREMENT'
-        })
-    }
-    const onIncrease = () => {
-        dispatch({
-            type: 'DECREMENT'
-        })
-    }
-    return (
-        <div>
-            <h1>{number}</h1>
-            <button onClick={onDecrease}>+1</button>
-            <button onClick={onIncrease}>-1</button>
-        </div>
-    );
-}
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './components/Hello';
+import Counter from './components/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Hello name="react" isSpecial="true"/>
+    <Counter name="react" isSpecial="true"/>
 );
 
 // If you want to start measuring performance in your app, pass a function
