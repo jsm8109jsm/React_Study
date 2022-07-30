6bf9f7a033078ae64acdb55fcab2cfe702acb900
jsm8109jsm <jsm8109jsm@gmail.com>
2022-07-30 15:20:42 +0900

    ESLint

--- useful-tools/src/App.js
+++ useful-tools/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+/* esLint-disable */
+
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+    const [value, setValue] = useState(0);
+    useEffect(() => {
+        console.log(value);
+    }, [value]);
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <div>asdfasfd</div>
+            <ul>
+                <li>asdfafdszxcv</li>
+            </ul>
         </div>
     );
 }
