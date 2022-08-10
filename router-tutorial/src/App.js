1b0ec348d1382ea59a0b168af096ce4f9bac4cb8
jsm8109jsm <jsm8109jsm@gmail.com>
2022-08-10 13:12:08 +0900

    라우팅 기본

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,28 @@
+//App.js
+
+import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ router-tutorial/src/components/About.js
@@ -0,0 +1,14 @@
+//About.js
+
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>이 프로젝트는 리액트 라우터 기초를 실습해보는 예제입니다.</p>
+    </div>
+  );
+}
+
+export default About;
