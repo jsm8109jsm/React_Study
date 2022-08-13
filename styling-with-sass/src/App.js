7fb0b49c9d15d4ffa7515f68927897872524f65a
jsm8109jsm <jsm8109jsm@gmail.com>
2022-08-13 21:18:35 +0900

    color props 설정

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -10,6 +10,16 @@ function App() {
         <Button>BUTTON</Button>
         <Button size="small">BUTTON</Button>
       </div>
+      <div className="buttons">
+        <Button color="gray" size="large">BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">BUTTON</Button>
+      </div>
     </div>
   );
 }
--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -3,12 +3,13 @@ import React from "react";
 import "./Button.scss";
 
 // size: large, medium, small
-function Button({ children, size }) {
-  return <button className={classNames("Button", size)}>{children}</button>;
+function Button({ children, size, color }) {
+  return <button className={classNames("Button", size, color)}>{children}</button>;
 }
 
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
 };
 
 export default Button;
