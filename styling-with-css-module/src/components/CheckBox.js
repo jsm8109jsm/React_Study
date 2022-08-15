f089c8881aff18b30b817a1f1044dc89e0e8ff19
jsm8109jsm <jsm8109jsm@gmail.com>
2022-08-15 21:18:20 +0900

    css module

---
+++ styling-with-css-module/src/components/CheckBox.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { MdCheckBox, MdCheckBoxOutlineBlank } from "react-icons/md";
+import styles from "./CheckBox.module.css";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+function CheckBox({ checked, children, ...rest }) {
+  return (
+    <div className={cx('checkbox')}>
+      <label>
+        <input type="checkbox" checked={checked} {...rest} />
+        <div className={styles.icon}>
+          {checked ? (
+            <MdCheckBox className={styles.checked} />
+          ) : (
+            <MdCheckBoxOutlineBlank />
+          )}
+        </div>
+      </label>
+      <span>{children}</span>
+    </div>
+  );
+}
+
+export default CheckBox;
